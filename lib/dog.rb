bb83800594340ddfd5f95e37d82b5128d321d410
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-08-20 18:18:17 +0300

    commit tree 049865cd645a3f098190858f7887b440243e8104

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
-
+    # Setter
+    def name= (dog_name)
+        @name = dog_name
+    end
+    # Getter
+    def name
+        @name
+    end
+    # Setter
+    def breed= (breed_name)
+        @breed = breed_name
+    end
+    # Getter
+    def breed
+        @breed
+    end
 end
+fido = Dog.new
+fido.name = "Fido"
+puts fido.name
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
+puts snoopy.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-
+    # Setter
+    def name= (person_name)
+        @name = person_name
+    end
+    # Getter
+    def name
+        @name
+    end
 end
