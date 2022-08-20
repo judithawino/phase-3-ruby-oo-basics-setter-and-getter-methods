c8b9af44ad7d25e74e3e0d68abc1000d64436b59
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-08-20 18:20:18 +0300

    Completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-
+    # Setter
+    def name= (person_name)
+        @name = person_name
+    end
+    # Getter
+    def name
+        @name
+    end
+    # Setter job
+    def job= (job_category)
+        @job = job_category
+    end
+    # Getter
+    def job
+        @job
+    end
 end
